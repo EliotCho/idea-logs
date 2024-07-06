6767d2e3e9ae3c3e8c94636c2a78fb40fd5452b0
Eliot Cho <eliotcho99@gmail.com>
2024-07-06 18:35:48 -0400

    css added different screen size, javascript not working

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,32 @@
-function themeChange() {
-    var element = document.main;
-    element.classList.toggle("dark-mode");
- }
+// render data from local storage
+
+
+// render the data from local storage
+function renderData() {
+  // get the data from local storage
+  const tempStorageObject = JSON.parse(localStorage.getItem('tempStorageObject'));
+
+
+  ideaList.innerHTML = '';
+
+
+
+
+
+
+
+
+
+
+  // check if the data is not null
+  if (tempStorageObject !== null) {
+    // loop through the text array and render the text
+    tempStorageObject.text.forEach((text) => {
+      const p = document.createElement('p');
+      p.textContent = text.text;
+      p.style.left = text.left;
+      p.style.top = text.top;
+      moodBoardEl.appendChild(p);
+    });
+  }
+}
