7cf46ffc70213bc24d79c4185dbeddf4fd93ca52
Eliot Cho <eliotcho99@gmail.com>
2024-07-08 02:47:13 -0400

    theme change color added for css, doesn't save the changed theme when moved to different html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,48 +1,40 @@
-const displayIdea = function () {
-  const ideas = readIdea();
-  for (const idea of ideas) {
-    console.log(idea);
-    // const section = document.createElement("section")
-    // const h2 = document.createElement("h2")
-    // const p = document.createElement("p")
-    // const h3 = document.createElement("h3")
-    // h2.textContent = idea.title
-    // p.textContent = idea.content
-    // h3.textContent = idea.userName
-    // section.appendChild(h2)
-    // h2.appendChild(p)
-    // p.appendChild(h3)
-    // ideaList.appendChild(section)
-  }
-};
+// const h2 = document.createElement("h2");
+// const p = document.createElement("p");
+// const h3 = document.createElement("h3");
 
 function renderData() {
+  console.log("renderData is running");
   // get the data from local storage
-  const tempStorageObject = JSON.parse(
-    localStorage.getItem("tempStorageObject")
-  );
+  let tempStorageObject =
+    JSON.parse(localStorage.getItem("tempStorageObject")) || [];
 
-  ideaList.innerHTML = "";
-  ideaListLength = tempStorageObject.length; //ideaListLength.textContent
-  console.log("tempStorageObject");
-  console.log(localStorage);
-  for (let i = 0; i < tempStorageObject.length; i++) {
-    const displayIdea = tempStorageObject[i];
+  // if there is no data, return
+  if (tempStorageObject.length === 0) return;
 
-    const section = document.createElement("section");
+  // ideaList.innerHTML = "";
+
+  // loop through the data
+  if (tempStorageObject.length > 0) {
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
     const h3 = document.createElement("h3");
-    h2.textContent = displayIdea.title;
-    p.textContent = displayIdea.content;
-    h3.textContent = displayIdea.userName;
-    h2.setAttribute("idea-title", h2.textContent);
-    p.setAttribute("idea-description", p.textContent);
-    h3.setAttribute("username", h3.textContent);
+    const section = document.createElement("section");
+    const ideaList = document.querySelector(".main-blog");
+    tempStorageObject.forEach((tempStorageObject) => {
+      h2.textContent = tempStorageObject.title;
+      p.textContent = tempStorageObject.content;
+      h3.textContent = tempStorageObject.userName;
 
-    section.appendChild(h2);
-    h2.appendChild(p);
-    p.appendChild(h3);
-    ideaList.appendChild(section);
+      h2.setAttribute("idea-title", h2.textContent);
+      p.setAttribute("idea-description", p.textContent);
+      h3.setAttribute("username", h3.textContent);
+
+      ideaList.appendChild(section);
+      section.appendChild(h2);
+      section.appendChild(p);
+      section.appendChild(h3);
+    });
+  } else {
+    console.log("No data to render");
   }
 }
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,12 @@
 const content_submit = document.querySelector("#content-submit");
-// const ideaList = document.querySelector(".main-blog");
 
-// const tempStorageObject = [];
+// get the data from local storage
+let tempStorageObject =
+  JSON.parse(localStorage.getItem("tempStorageObject")) || [];
 
-let tempStorageObjects = localStorage.getItem("tempStorageObject");
-let tempStorageObject = tempStorageObjects
-  ? JSON.parse(tempStorageObjects)
-  : [];
-
-const formSubmit = function (event) {
+// function formSubmit() {
+// const formSubmit = function (event) {
+function formSubmit(event) {
   event.preventDefault();
 
   const userName = document.querySelector("#username").value.trim();
@@ -16,6 +14,7 @@ const formSubmit = function (event) {
   const content = document.querySelector("#idea-description").value.trim();
   const submissionResponseEl = document.querySelector("#response");
 
+  // if the form is incomplete, return
   if (userName == "" || userName.trim() == " ") {
     submissionResponseEl.textContent =
       "Username incomplete, please fill out the form";
@@ -37,14 +36,24 @@ const formSubmit = function (event) {
       content: content,
     };
 
+    console.log(article.userName);
+    console.log(article.title);
+    console.log(article.content);
+
     tempStorageObject.push(article);
     localStorage.setItem(
       "tempStorageObject",
       JSON.stringify(tempStorageObject)
     );
     renderData();
+    // document.querySelector("#username").value = "";
+    // document.querySelector("#idea-title").value = "";
+    // document.querySelector("#idea-description").value = "";
     // localStorage.clear();
+    // setTimeout(() => {
+    //   window.location.href = "./blog.html";
+    // }, 100);
   }
-};
+}
 
 content_submit.addEventListener("click", formSubmit);
